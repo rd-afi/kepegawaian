fcf02c7d17d5f6677fbb415ce53314a83745fd49
Raafi'ud Darajaat <dtsevine@gmail.com>
2018-04-12 15:37:59 +0700

    ...

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -79,6 +79,10 @@
                 <ul class="treeview-menu">
                     <li><a href="<?php echo site_url('akun') ?>"><i class="fa fa-circle-o"></i> Akun</a></li>
                 </ul>
+                <ul class="treeview-menu">
+                    <li><a href="<?php echo site_url('akun/tambahacc') ?>"><i class="fa fa-circle-o"></i>Buat Akun</a></li>
+                </ul>
+            </li>
             </li>
             <!--
             <li><a href="#"><i class="fa fa-book"></i> Documentation</a></li>
